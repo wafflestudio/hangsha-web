dce4d9170dc30052c4bb7abdd6e78dcc84935094
Seoyeon HEO <best3487@snu.ac.kr>
2026-04-16 22:01:48 +0900

    feat: add OpenLnL category

--- src/util/constants/index.ts
+++ src/util/constants/index.ts
@@ -1,19 +1,33 @@
 export const CATEGORY_COLORS: Record<number, string> = {
-	1: "#DFD093",
-	2: "#69DEB1",
-	3: "#72D3FA",
-	4: "#62B4FB",
-	5: "#C398FB",
-	6: "#FB7D95",
+	// 1: "#DFD093",
+	// 2: "#69DEB1",
+	// 3: "#72D3FA",
+	// 4: "#62B4FB",
+	// 5: "#C398FB",
+	// 6: "#FB7D95",
+	1: "#FBB77A",
+	2: "#DFD093",
+	3: "#69DEB1",
+	4: "#72D3FA",
+	5: "#62B4FB",
+	6: "#C398FB",
+	7: "#FB7D95",
 };
 
 export const CATEGORY_BUTTON_COLORS: Record<number, string> = {
-	1: "#ffeea962",
-	2: "#69deb15b",
-	3: "#72d3fa62",
-	4: "#629ffb5f",
-	5: "#c398fb62",
-	6: "#fb7d945e",
+	// 1: "#ffeea962",
+	// 2: "#69deb15b",
+	// 3: "#72d3fa62",
+	// 4: "#629ffb5f",
+	// 5: "#c398fb62",
+	// 6: "#fb7d945e",
+	1: "#fbb67a5f",
+	2: "#ffeea962",
+	3: "#69deb15b",
+	4: "#72d3fa62",
+	5: "#629ffb5f",
+	6: "#c398fb62",
+	7: "#fb7d945e",
 };
 
 export const CATEGORY_LIST: Record<number, string> = {
@@ -22,5 +36,6 @@ export const CATEGORY_LIST: Record<number, string> = {
 	3: "현장학습/인턴",
 	4: "사회공헌(봉사)",
 	5: "학습/진로상담",
-	6: "기타",
+	6: "OpenLnL",
+	7: "기타",
 };
